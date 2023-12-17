589206a10da8a296408e53b2b086bc88a6cabbc9
Kalzak <kalzak.dev@protonmail.com>
2023-12-17 20:45:09 +0800

    improved claimer calculation logic

--- src/Claimer.sol
+++ src/Claimer.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.20;
 import { VestClaimFactory } from "./VestClaimFactory.sol";
 import { VestingAgreement } from "./VestingAgreement.sol";
 
+import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
 contract Claimer {
 
     uint256 constant monthsInSeconds = 2628000;
@@ -11,56 +13,45 @@ contract Claimer {
         // Get the vestTokenAddr address
         VestingAgreement vestTokenAddr = VestClaimFactory(msg.sender).vestTokenAddr();
         
+        // Get the beneficiary address
         address beneficiary = vestTokenAddr.ownerOf(address(this));
 
         // Get the vesting parameters
         (
             uint128 cliffMonths,
             uint128 vestMonths,
-            uint128 startTime128,
+            uint128 startTime_u128,
             uint128 amount
         ) = vestTokenAddr.contractDetails(beneficiary);
 
-        
-
-        uint256 cliffTime = cliffMonths * 2628000; // Seconds in a non-leap-year div 12
-        uint256 startTime = uint256(startTime128);
+        uint256 remainingAmount = address(this).balance;
 
         // Vesting must have started
+        uint256 startTime = startTime_u128;
         require(block.timestamp >= startTime, "!started");
 
-        // Must have passed cliff window
-        require(block.timestamp >= startTime + cliffTime, "incliffwindow");
+        // Must have passed cliff period
+        require(block.timestamp >= startTime + (cliffMonths * monthsInSeconds), "incliff");
 
-        // Calculate how many months haven't been paid yet
-        uint256 amountPerMonth = amount / (cliffMonths + vestMonths);
-        uint256 monthsUnpaid = address(this).balance / amountPerMonth;
-
-        // If no unpaid months then entire contract wait time is done
-        if(monthsUnpaid == 0) {
-            send(beneficiary, address(this).balance);
+        // If entire vest period has passed send all funds
+        if(block.timestamp >= startTime + ((cliffMonths + vestMonths) * monthsInSeconds)) {
+            send(beneficiary, remainingAmount);
+            return;
         }
 
-        // Calculate how many months have been paid
-        uint256 monthsPaid = (cliffMonths + vestMonths) - monthsUnpaid;
+        // Send funds for unpaid months
+        uint256 currentMonth = (block.timestamp - startTime) / monthsInSeconds;
+        uint256 amountPerMonth = amount / (cliffMonths + vestMonths);
+        uint256 paidMonths = (amount - remainingAmount) / amountPerMonth;
+        uint256 amountToPay = (currentMonth - paidMonths) * amountPerMonth;
 
-        // Calculate the current month we're at
-        uint256 timeSinceStart = block.timestamp - startTime;
-        uint256 currentMonth = timeSinceStart / monthsInSeconds;
-
-        // Calculate the number of months owed
-        uint256 monthsOwed = currentMonth - monthsPaid;
-
-        // Pay owed months
-        send(beneficiary, amountPerMonth * monthsOwed);
+        send(beneficiary, amountToPay);
 
         selfdestruct(payable(address(this)));
-
     }
 
     function send(address beneficiary, uint256 amount) internal {
-        (bool success, bytes memory data) = beneficiary.call{value: amount, gas: 10000}("");
+        (bool success, ) = beneficiary.call{value: amount, gas: 10000}("");
         require(success, "failed");
-        require(data.length == 0, "len");
     }
 }
